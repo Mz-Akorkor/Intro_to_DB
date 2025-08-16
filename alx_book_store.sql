b5697859b9ca11065292921e4e6dce22ca01be05
MzAkorkor <mzakorkor@gmail.com>
2025-08-16 23:21:04 +0000

    Update FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,7 +32,7 @@ CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
 
 -- Order_Details Table
@@ -41,6 +41,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
