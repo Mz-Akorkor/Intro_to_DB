bb8f45538c14896d1cf339f76bfc3fdd494adb8c
MzAkorkor <mzakorkor@gmail.com>
2025-08-17 15:29:49 +0000

    Delete connect_alxbookstore.py

--- connect_alxbookstore.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="MySQL1918",
-    database="alx_book_store"
-)
-print("Connected to:", mydb.server_info)
