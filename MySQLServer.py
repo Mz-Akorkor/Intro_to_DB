ded146e2d9b7d50206ed98d437d1fa0c4d421bf4
MzAkorkor <mzakorkor@gmail.com>
2025-08-17 13:51:48 +0000

    Updated CREATE DATABASE IF NOT EXISTS alx_book_store

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,17 +2,25 @@ import os
 import mysql.connector
 from mysql.connector import Error
 
+
 try:
+    # Connect to MySQL server (not to a specific database yet)
     connection = mysql.connector.connect(
         host="localhost",
         user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        database="alx_book_store"
+        password=os.getenv("MYSQL_PASSWORD")
     )
+
     if connection.is_connected():
-        print("Connected to MySQL server successfully!")
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+        print("Database 'alx_book_store' created successfully!")
+
 except Error as e:
     print("Error while connecting to MySQL:", e)
+
 finally:
     if 'connection' in locals() and connection.is_connected():
+        cursor.close()
         connection.close()
+        print("MySQL connection is closed.")
